3deb59eca64e8bca070f53711d33f8ca9fdc13f7
Visvesh Khanna <visveshkhanna@gmail.com>
2025-04-13 23:33:24 +0530

    feat: Clerk + Prisma (Postgres) Setup

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,14 +1,22 @@
+"use client";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export default function Provider({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <ClerkProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
