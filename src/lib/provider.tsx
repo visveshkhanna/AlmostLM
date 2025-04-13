4da56950bfc42ce2ca282efe873562e480553709
Visvesh Khanna <visveshkhanna@gmail.com>
2025-04-13 23:15:28 +0530

    feat: initial commit
    NextJS 15 + ShadCN Setup

---
+++ src/lib/provider.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
